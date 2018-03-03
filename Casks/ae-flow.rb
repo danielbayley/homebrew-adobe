f410711f82b83511b4b809c67a0a374d101fd948
Daniel Bayley <daniel.bayley@me.com>
2018-03-03 22:48:52 +0000

    Add Flow

---
+++ Casks/ae-flow.rb
@@ -0,0 +1,26 @@
+cask 'ae-flow' do
+  version :latest
+  sha256 :no_check
+
+  # aescripts.com was verified as official when first introduced to the cask
+  url 'https://aescripts.com/downloadable/download/sample/sample_id/454',
+      user_agent: :fake
+  name 'Flow'
+  homepage 'https://zacklovatt.com/tools/flow'
+
+  depends_on formula: 'exmancmd'
+
+  postflight do
+    zxp = Dir["#{staged_path}/flow-v*/Flow v* (CC2015.2+)/Flow.zxp"][0]
+    system_command "#{HOMEBREW_PREFIX}/bin/exmancmd", args: ['--install', zxp]
+  end
+
+  uninstall script: {
+                      executable: "#{HOMEBREW_PREFIX}/bin/exmancmd",
+                      args:       ['--remove', 'Flow'],
+                    }
+
+  zap delete: '~/Library/Application Support/com.aescripts.flow.lic'
+
+  caveats 'A license can be purchased at https://aescripts.com/flow.'
+end
