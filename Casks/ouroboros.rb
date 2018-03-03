9813d2e0656498c73f3b169c363567552a39bb79
Daniel Bayley <daniel.bayley@me.com>
2018-03-03 23:12:19 +0000

    Add Ouroboros 2.0.2

---
+++ Casks/ouroboros.rb
@@ -0,0 +1,26 @@
+cask 'ouroboros' do
+  version '2.0.2'
+  sha256 '093f971a6dad4670a4da361e854dc104e31ee5ad40a05a6dd5c94ba3391fe470'
+
+  # dropbox.com/s/9uv7vu4amz80eek was verified as official when first introduced to the cask
+  url "https://dropbox.com/s/9uv7vu4amz80eek/ouroboros_#{version}.zip?dl=1"
+  name 'Ouroboros'
+  homepage 'http://georegulus.com/'
+
+  folder = '/Applications/Adobe Creative Cloud/Adobe After Effects */Scripts/ScriptUI Panels/'
+  plugin = 'Ouroboros.jsxbin'
+
+  latest = Dir[folder].sort { |min, max| min <=> max }[-1]
+
+  artifact plugin, target: "#{latest}/#{plugin}"
+
+  uninstall delete: [
+                      folder + plugin,
+                      '~/Library/Application Support/Ouroboros',
+                    ]
+
+  caveats do
+    free_license "https://aescripts.com/ouroboros-#{version.major}"
+    'Please consider a voluntary purchase if you like this plugin.'
+  end
+end
