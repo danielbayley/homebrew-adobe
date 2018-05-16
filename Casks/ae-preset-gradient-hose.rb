493e5d10c0633c98dac4c500f493ac5c867a0c97
Daniel Bayley <daniel.bayley@me.com>
2018-05-16 23:01:04 +0100

    Update Gradient Hose

---
+++ Casks/ae-preset-gradient-hose.rb
@@ -0,0 +1,18 @@
+cask 'ae-preset-gradient-hose' do
+  version '1.0'
+  sha256 '97992fa041084bd811f06162b4ffc3df62cae3f7425cd5b66c5c7436c8e713e5'
+
+  url 'http://battleaxe.co/s/GradientHose_Style.zip'
+  name 'Gradient Hose'
+  homepage 'http://battleaxe.co/rh2-addons#gradient-hose'
+
+  depends_on cask: 'rubberhose'
+
+  root = File.expand_path '~/Library/Application Support/BattleAxe'
+  folder = Dir["#{root}/RubberHose */Styles"][0]
+
+  preset = 'Gradient Hose.ffx'
+  artifact preset, target: "#{folder}/#{preset}"
+
+  uninstall rmdir: root
+end
