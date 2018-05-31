1fbad0e7f17bf84df481a551c787ee59e5e7211b
Daniel Bayley <daniel.bayley@me.com>
2018-05-31 20:57:48 +0100

    Improve Expressionist

--- Casks/expressionist.rb
+++ Casks/expressionist.rb
@@ -6,8 +6,9 @@ cask 'expressionist' do
   url 'https://aescripts.com/downloadable/download/sample/sample_id/411',
       user_agent: :fake
   name 'Expressionist'
-  homepage 'http://klustre.nl/expressionist'
+  homepage "http://klustre.nl/#{token}"
 
+  auto_updates true
   depends_on formula: 'exmancmd'
 
   postflight do
@@ -20,7 +21,12 @@ cask 'expressionist' do
                       args:       ['--remove', 'com.aescripts.expressionist'],
                     }
 
-  zap delete: '~/Library/Preferences/com.aescripts.Expressionist.lic'
+  support = '~/Library/Application Support/Aescripts'
+  zap trash: [
+               "#{support}/settings/com.aescripts.settings_Expressionist.json",
+               "#{support}/com.aescripts.Expressionist.lic",
+             ],
+      rmdir: support
 
   caveats "A license can be purchased at https://aescripts.com/#{token}."
 end
