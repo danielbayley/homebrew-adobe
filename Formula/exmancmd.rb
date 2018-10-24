3a7592b6b0dc3e2d38821050eb5a969c3176aa00
Daniel Bayley <daniel.bayley@me.com>
2018-10-24 23:30:02 +0100

    Update Extension Manager CLI to 7.5.0.79

--- Formula/exmancmd.rb
+++ Formula/exmancmd.rb
@@ -2,8 +2,8 @@ class Exmancmd < Formula
   desc "Install add-ons and extensions for Adobe apps"
   homepage "https://www.adobeexchange.com/resources/28"
   url "https://adobe.com/go/ExManCmdMac"
-  version "7.4.0.88"
-  sha256 "6a20d9a4ad606a0f8e481db80ee5c37da367e2d31f4ebff2a1bc283bec36deec"
+  version "7.5.0.79"
+  sha256 "bf092874723f1a61c7ca76db0a9f75a8a65ae805ef6568cf54c8189b6c40b507"
 
   bottle :unneeded
 
@@ -13,6 +13,6 @@ class Exmancmd < Formula
   end
 
   test do
-    system bin/"ExManCmd", "--help"
+    assert_match version.to_s, shell_output("otool -P #{bin}/ExManCmd")
   end
 end
