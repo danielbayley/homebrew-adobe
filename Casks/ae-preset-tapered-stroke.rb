c1fe2b8ac41dd35a42769494d1c1fd3a54f1bf7e
Daniel Bayley <daniel.bayley@me.com>
2019-04-30 17:08:12 +0100

    Fix Tapered Stroke 1.0.2

--- Casks/ae-preset-tapered-stroke.rb
+++ Casks/ae-preset-tapered-stroke.rb
@@ -1,14 +1,14 @@
 cask 'ae-preset-tapered-stroke' do
-  version '1.0'
+  version '1.0.2'
   sha256 '03500b99d0feed2007ad4d0de009627c84f9fa87f88ef58d2bc30ddc4cd09bae'
 
   # sommediaprod.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://sommediaprod.s3.amazonaws.com/284034a4-39a2-4be1-a242-40ebed307582.zip'
+  url "https://static.schoolofmotion.com/50c63144-82a6-4fdd-92d4-c1126f276290/TaperedStroke_v#{version.major_minor}_v#{version.patch}.zip"
   name 'Tapered Stroke'
   homepage 'http://schoolofmotion.com/tutorials/free-tapered-stroke-after-effects'
 
   folder = Dir['/Applications/Adobe Creative Cloud/Adobe After Effects */Presets'].max
 
-  preset = "TaperedStroke_v#{version}.ffx"
+  preset = "TaperedStroke_v#{version.major_minor}.ffx"
   artifact preset, target: "#{folder}/#{preset}"
 end
