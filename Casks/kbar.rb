d6782ae67b15914c43558d3525fb77ec20712272
Daniel Bayley <daniel.bayley@me.com>
2019-04-30 17:57:29 +0100

    Fix KBar

--- Casks/kbar.rb
+++ Casks/kbar.rb
@@ -2,7 +2,7 @@ cask 'kbar' do
   version :latest
   sha256 :no_check
 
-  url 'https://aescripts.com/downloadable/download/sample/sample_id/499',
+  url 'https://aescripts.com/downloadable/download/sample/sample_id/677',
       user_agent: :fake
   name 'KBar'
   homepage 'https://aescripts.com/kbar'
