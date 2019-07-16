185d6cbd9e241038a8c3e71bf6108ffb5234e1f7
Daniel Bayley <daniel.bayley@me.com>
2019-07-16 01:20:00 +0100

    Update Overlord to 1.20

--- Casks/overlord.rb
+++ Casks/overlord.rb
@@ -1,18 +1,17 @@
 cask 'overlord' do
-  version '1.12'
-  sha256 'a39a5e333f1aa334f2080bab388ef138d879a7addab4de3952b2784af1bf4512'
+  version '1.20'
+  sha256 'fc4fbad3fbada2c58658c4bb76160aacf95df71af022e17daf20cce43ee5a252'
 
   # dropbox.com/s was verified as official when first introduced to the cask
   url "https://dropbox.com/s/#{ENV['HOMEBREW_ADOBE_OVERLORD']}/Overlord-#{version}.zip?dl=1"
   name 'Overlord'
   homepage 'https://battleaxe.co/overlord'
 
-  # auto_updates true
   depends_on formula: 'exmancmd'
 
   installer script: {
-                      executable: "#{HOMEBREW_PREFIX}/bin/exmancmd", # /01. Updatable Version/Overlord.zxp
-                      args:       ['--install', "#{staged_path}/Overlord-#{version}/02. Stable Version/Overlord-stable.zxp"],
+                      executable: "#{HOMEBREW_PREFIX}/bin/exmancmd",
+                      args:       ['--install', "#{staged_path}/Overlord-#{version}/Overlord.zxp"],
                     }
 
   uninstall script: {
