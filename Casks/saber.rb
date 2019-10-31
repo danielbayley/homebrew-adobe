3e6c048e28ebb6e166c121f72c0eccdea4a77874
Daniel Bayley <daniel.bayley@me.com>
2019-10-31 23:32:04 +0000

    Fix Saber

--- Casks/saber.rb
+++ Casks/saber.rb
@@ -1,19 +1,15 @@
 cask 'saber' do
   version '1.0.39,2018'
-  sha256 '3ecba4d2f1e658d89aabce57ea4916f034b286814ba87a982242ee7b85b063d2'
+  sha256 '8223f8c014dfa30a25223eaf393921f10e5a39dbc1251069f45c9c906a86c848'
 
-  # vcproducts.s3.amazonaws.com/free was verified as official when first introduced to the cask
+  # vcproducts.s3.amazonaws.com/free/saber was verified as official when first introduced to the cask
   url "https://vcproducts.s3.amazonaws.com/free/saber/SaberInstaller_#{version.before_comma}_Mac_#{version.after_comma}.zip"
   name 'Saber'
-  homepage 'https://videocopilot.net/blog/?s=saber'
+  homepage 'https://videocopilot.net/tutorials/saber_plug-in'
 
-  container nested: "SaberInstaller_#{version.before_comma}_Mac.dmg"
-
-  folder = Dir['/Applications/Adobe Creative Cloud/Adobe After Effects *'].max
-
-  plugin = 'Saber.plugin'
-  artifact "SaberInstaller.app/Contents/Resources/#{plugin}",
-           target: "#{folder}/Plug-ins/VideoCopilot/#{plugin}"
+  folder = Dir["#{appdir}/Adobe After Effects *"].max
+  artifact 'SaberInstaller.app/Contents/Resources/Saber.plugin',
+           target: "#{folder}/Plug-ins/VideoCopilot/Saber.plugin"
 
   uninstall rmdir: "#{folder}/Plug-ins/VideoCopilot"
 end
