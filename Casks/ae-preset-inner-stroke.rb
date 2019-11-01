fe8cfb135aa356f73798dd1c38740189b3ea8a09
Daniel Bayley <daniel.bayley@me.com>
2019-11-01 01:44:37 +0000

    Fix Inner Stroke

--- Casks/ae-preset-inner-stroke.rb
+++ Casks/ae-preset-inner-stroke.rb
@@ -3,13 +3,13 @@ cask 'ae-preset-inner-stroke' do
   sha256 :no_check
 
   preset = 'zl_InnerStroke.ffx'
-  url "https://zacklovatt.com/presets/#{preset}"
+  url "https://zacklovatt.com/presets/#{preset}", user_agent: :fake
   name 'Inner Stroke'
   homepage 'https://zacklovatt.com/preset-shape-layer-inner-stroke'
 
   container type: :naked
 
-  folder = Dir['/Applications/Adobe Creative Cloud/Adobe After Effects */Presets'].max
+  folder = Dir["#{appdir}/Adobe After Effects */Presets"].max
 
   artifact preset, target: "#{folder}/#{preset}"
 end
